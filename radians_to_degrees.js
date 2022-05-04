bb3a7a504d65daf8bdee623400fdad3d5f6ec38c
Karolina Gugudis <102053483+KaroGugu@users.noreply.github.com>
2022-05-04 15:13:55 +0300

    Add files via upload

---
+++ radians_to_degrees.js
@@ -0,0 +1,7 @@
+function radians_to_degrees(input){
+    let radians = Number(input[0]);
+    let degrees = radians * 180 / Math.PI;
+    console.log(degrees)
+}
+
+// radians_to_degrees((["6.2832"]))
