f32fa927709187b27502a6c690fd78024177bbde
Karolina Gugudis <102053483+KaroGugu@users.noreply.github.com>
2022-05-25 15:07:17 +0300

    Add files via upload

--- dayOfWeek.js
+++ dayOfWeek.js
@@ -1,23 +1,47 @@
-function dayOfWeek(input){
-    let number = Number(input[0]);
+// function dayOfWeek(input){
+//     let number = Number(input[0]);
 
-    if (number == "1"){
-        console.log("Monday")
-    } else if (number == "2"){
-        console.log("Tuesday")
-    }else if (number == "3"){
-        console.log("Wednesday")
-    }else if (number == "4"){
-        console.log("Thursday")
-    } else if (number == "5"){
-        console.log("Friday")
-    } else if (number == "6"){
-        console.log("Saturday")
-    } else if (number == "7"){
-        console.log("Sunday")
-    } else {
-        console.log("Error")
+//     if (number == "1"){
+//         console.log("Monday")
+//     } else if (number == "2"){
+//         console.log("Tuesday")
+//     }else if (number == "3"){
+//         console.log("Wednesday")
+//     }else if (number == "4"){
+//         console.log("Thursday")
+//     } else if (number == "5"){
+//         console.log("Friday")
+//     } else if (number == "6"){
+//         console.log("Saturday")
+//     } else if (number == "7"){
+//         console.log("Sunday")
+//     } else {
+//         console.log("Error")
+//     }
+// }
+
+// // dayOfWeek((["-1"]))
+
+function dayofweek(input){
+    number = Number(input[0]);
+    switch (number){
+        case 1:
+            console.log("Monday"); break;
+        case 2:
+            console.log("Tuesday"); break;
+        case 3:
+            console.log("Wednesday"); break;
+        case 4:
+            console.log("Thursday"); break;
+        case 5:
+            console.log("Friday"); break;
+        case 6:
+            console.log("Saturday"); break;
+        case 7:
+            console.log("Sunday"); break;        
+        default:
+            console.log("Error"); break;        
     }
 }
 
-// dayOfWeek((["-1"]))
+dayofweek([-1])
