da0c01a3ce2677d5b3797f7ee3ab2f437c8d7cfd
Karolina Gugudis <102053483+KaroGugu@users.noreply.github.com>
2022-05-30 13:58:41 +0300

    Add files via upload

---
+++ fruitorVegetable.js
@@ -0,0 +1,13 @@
+function fruitorvegetable(input){
+    let food = input[0];
+
+    if (food == "banana" || food == "apple" || food == "kiwi" || food == "cherry" || food == "lemon" || food == "grapes"){
+        console.log("fruit")
+    } else if (food == "tomato" || food == "cucumber" || food == "pepper" || food == "carrot"){
+        console.log("vegetable")
+    } else {
+        console.log("unknown")
+    }
+}
+
+// fruitorvegetable((["water"]))
