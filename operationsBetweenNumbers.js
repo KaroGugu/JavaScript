31eeee7bb0b2a0f42931c0219efcd8ceb12a4009
Karolina Gugudis <102053483+KaroGugu@users.noreply.github.com>
2022-06-02 15:11:56 +0300

    Add files via upload

--- operationsBetweenNumbers.js
+++ operationsBetweenNumbers.js
@@ -2,46 +2,96 @@ function operationsBetweenNumbers(input){
     let firstnumber = Number(input[0]);
     let secondnumber = Number(input[1]);
     let operator = input[2];
-
+    
     let result = 0
 
-    if (operator == "+"){
-        result = firstnumber + secondnumber
-        if (result % 2 == 0){
-            console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "even")
-        } else{
-            console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "odd")
-        }
-    } else if (operator == "-"){
-        result = firstnumber - secondnumber
-        if (result % 2 == 0){
-            console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "even")
-        } else{
-            console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "odd")
-        }
-    } else if (operator == "*"){
-        result = firstnumber * secondnumber
-        if (result % 2 == 0){
-            console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "even")
-        } else{
-            console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "odd")
-        }
-    } else if (operator == "/"){
-        if (secondnumber == 0){
-            console.log("Cannot divide " + firstnumber + " by zero")
-        } else {
-            result = firstnumber / secondnumber
-            console.log(firstnumber + " / " + secondnumber + " = " + result)
-        }
-    } else if (operator == "%"){
-        if (secondnumber == 0){
-            console.log("Cannot divide " + firstnumber + " by zero")
-        } else {
-            result = firstnumber % secondnumber
-            console.log(firstnumber + " % " + secondnumber + " = " + result)
-        }
+//     if (operator == "+"){
+//         result = firstnumber + secondnumber
+//         if (result % 2 == 0){
+//             console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "even")
+//         } else{
+//             console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "odd")
+//         }
+//     } else if (operator == "-"){
+//         result = firstnumber - secondnumber
+//         if (result % 2 == 0){
+//             console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "even")
+//         } else{
+//             console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "odd")
+//         }
+//     } else if (operator == "*"){
+//         result = firstnumber * secondnumber
+//         if (result % 2 == 0){
+//             console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "even")
+//         } else{
+//             console.log (firstnumber + " " + operator + " " + secondnumber + " = " + result + " - " + "odd")
+//         }
+//     } else if (operator == "/"){
+//         if (secondnumber == 0){
+//             console.log("Cannot divide " + firstnumber + " by zero")
+//         } else {
+//             result = firstnumber / secondnumber
+//             console.log(firstnumber + " / " + secondnumber + " = " + result.toFixed(2))
+//         }
+//     } else if (operator == "%"){
+//         if (secondnumber == 0){
+//             console.log("Cannot divide " + firstnumber + " by zero")
+//         } else {
+//             result = firstnumber % secondnumber
+//             console.log(firstnumber + " % " + secondnumber + " = " + result)
+//         }
 
-    }
+//     }
+// }
+
+
+
+
+switch(operator){
+    case "+": 
+       result = firstnumber + secondnumber; 
+       if (result % 2 === 0){
+           console.log(`${firstnumber} ${operator} ${secondnumber} = ${result} - even`);
+       } else{
+           console.log(`${firstnumber} ${operator} ${secondnumber} = ${result} - odd`);
+       }
+       break;
+
+    case "-": 
+        result = firstnumber - secondnumber; 
+       if (result % 2 === 0){
+           console.log(`${firstnumber} ${operator} ${secondnumber} = ${result} - even`);
+       } else{
+           console.log(`${firstnumber} ${operator} ${secondnumber} = ${result} - odd`);
+       } break;
+
+    case "*": 
+        result = firstnumber * secondnumber; 
+       if (result % 2 === 0){
+           console.log(`${firstnumber} ${operator} ${secondnumber} = ${result} - even`);
+       } else{
+           console.log(`${firstnumber} ${operator} ${secondnumber} = ${result} - odd`);
+       } break;
+
+    case "/": 
+        if (secondnumber === 0){
+            console.log(`Cannot divide ${firstnumber} by zero`);
+        } else{
+            result = firstnumber / secondnumber
+            console.log(`${firstnumber} / ${secondnumber} = ${result.toFixed(2)}`);
+        }
+        break;
+
+    case "%": 
+        if (secondnumber === 0){
+            console.log(`Cannot divide ${firstnumber} by zero`);
+        } else{
+            result = firstnumber % secondnumber
+            console.log(`${firstnumber} % ${secondnumber} = ${result}`);
+    } break;
+
+}
+
 }
 
 // operationsBetweenNumbers((["7",
